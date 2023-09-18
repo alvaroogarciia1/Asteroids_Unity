1ea913151a09faafa25d6200b04a715ccb9b0fe6
alvaroogarciia1 <127146967+alvaroogarciia1@users.noreply.github.com>
2023-09-18 22:45:43 +0200

    Actualizacion codigo fuente

---
+++ Asteroids/Assets/Scripts/Player.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+
+    public float thrustForce = 10f;
+    public float rotationSpeed = 120f;
+
+    public GameObject gun, bulletPrefab;
+
+    public static int PUNTUACION = 0;
+    public static float xLimit, yLimit;
+
+
+    private Rigidbody _rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody>();
+        yLimit = Camera.main.orthographicSize + 1;
+        xLimit = yLimit * Screen.width / Screen.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        MakeInfiniteSpace();
+        float rotation = Input.GetAxis("Rotate") * Time.deltaTime;
+
+        float thrust = Input.GetAxis("Thrust") * Time.deltaTime * thrustForce;
+
+        Vector3 thrustDirection = transform.right;
+
+        _rigid.AddForce(thrustDirection * thrust);
+
+        transform.Rotate(Vector3.forward, -rotation * rotationSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, gun.transform.position, Quaternion.identity);
+
+            Bullet balaScript = bullet.GetComponent<Bullet>();
+
+            balaScript.targetVector = transform.right;
+        }
+    }
+
+    private void MakeInfiniteSpace()
+    {
+        var newPosition = transform.position;
+        if (newPosition.x > xLimit)
+        {
+            newPosition.x = -xLimit + 1;
+        }
+        else if (newPosition.x < -xLimit)
+        {
+            newPosition.x = xLimit - 1;
+        }
+        else if (newPosition.y > yLimit)
+        {
+            newPosition.y = -yLimit + 1;
+        }
+        else if (newPosition.y < -yLimit)
+        {
+            newPosition.y = yLimit - 1;
+        }
+
+        transform.position = newPosition;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            PUNTUACION = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            Debug.Log("Hola");
+        }
+
+    }
+
+
+}
